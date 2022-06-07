388b8dc47313af07ad7a6cfeaf8fbc986cb4b6b4
Your Name <you@example.com>
2022-06-07 15:29:59 -0300

    BacteriaData

---
+++ src/Main/Extras/BacteriaData.java
@@ -0,0 +1,15 @@
+package Main.Extras;
+
+public class BacteriaData {
+    String code;
+    String Type;
+    String description;
+    int size;
+
+    public BacteriaData(String code, String type, String description, int size) {
+        this.code = code;
+        Type = type;
+        this.description = description;
+        this.size = size;
+    }
+}
